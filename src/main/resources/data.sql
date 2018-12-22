0420d9210eb9fcc80901f1e422aabfd13c81f711
ankit ranjan <ankit.ranjan@accolitelabs.com>
2018-12-22 17:19:41 +0530

    Adding few reserved / unreserved cars in the data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -40,3 +40,17 @@ values
  (8,
  'aced0005737200226f72672e737072696e676672616d65776f726b2e646174612e67656f2e506f696e7431b9e90ef11a4006020002440001784400017978704023000000000000404bfa1cac083127', now(), now(), false, 'ONLINE',
 'driver08pw', 'driver08');
+
+-- Create 3 reserved/selected cars
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (1, now(), 'BKW 1000', 4, false, 4.0, 'GAS', 'BMW', true);
+
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (2, now(), 'BKW 1001', 6, false, 3.0, 'GAS', 'BENZ', true);
+
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (3, now(), 'BKW 1002', 4, true, 5.0, 'ELECTRIC', 'DAIMLER', true);
+
+-- Create 3 un-selected/un-reserved cars
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (4, now(), 'BKW 1003', 4, false, 4.0, 'DIESEL', 'BMW', false);
+
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (5, now(), 'BKW 1004', 6, false, 3.0, 'GAS', 'BENZ', false);
+
+insert into car (id, date_created, license_plate, seat_count, convertible, rating, engine_type ,make, selected) values (6, now(), 'BKW 1005', 4, true, 5.0, 'ELECTRIC', 'AUDI', false);
